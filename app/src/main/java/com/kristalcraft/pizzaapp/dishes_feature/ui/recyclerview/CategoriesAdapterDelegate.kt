98b8d163516be4d1b8366305ea157f73ff44384f
Kristalcraft <dmbezoar@gmail.com>
2023-11-04 15:53:50 +0300

    fix shadows

--- app/src/main/java/com/kristalcraft/pizzaapp/dishes_feature/ui/recyclerview/CategoriesAdapterDelegate.kt
+++ app/src/main/java/com/kristalcraft/pizzaapp/dishes_feature/ui/recyclerview/CategoriesAdapterDelegate.kt
@@ -1,8 +1,10 @@
 package com.kristalcraft.pizzaapp.dishes_feature.ui.recyclerview
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.graphics.alpha
 import androidx.recyclerview.widget.RecyclerView
 import com.kristalcraft.delegate_adapter.DelegateAdapter
 import com.kristalcraft.delegate_adapter.DelegateAdapterItem
@@ -33,10 +35,14 @@ class CategoriesAdapterDelegate(val onCategoryClickListener: (name: String) -> U
                 binding.name.typeface= Typeface.create(null,600,false)
                 binding.name.setTextColor(binding.name.context.resources.getColor(R.color.red))
                 binding.background.setCardBackgroundColor(binding.name.context.resources.getColor(R.color.red_transparent))
+                binding.background.outlineSpotShadowColor = Color.TRANSPARENT
+                binding.background.outlineAmbientShadowColor = Color.TRANSPARENT
             } else {
                 binding.name.typeface= Typeface.create(null,400,false)
                 binding.name.setTextColor(binding.name.context.resources.getColor(R.color.grey_text))
                 binding.background.setCardBackgroundColor(binding.name.context.resources.getColor(R.color.white))
+                binding.background.outlineSpotShadowColor = binding.name.context.resources.getColor(R.color.grey_shadow)
+                binding.background.outlineAmbientShadowColor = binding.name.context.resources.getColor(R.color.grey_shadow)
             }
         }
     }
