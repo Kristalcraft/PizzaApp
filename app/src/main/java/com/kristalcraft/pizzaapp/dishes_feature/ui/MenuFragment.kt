cdb3cd1dc01a5227c91a8e92ccd3e4bcbb8b16d8
Kristalcraft <dmbezoar@gmail.com>
2023-11-04 03:12:18 +0300

    add recycler view adapters to fragment

--- app/src/main/java/com/kristalcraft/pizzaapp/dishes_feature/ui/MenuFragment.kt
+++ app/src/main/java/com/kristalcraft/pizzaapp/dishes_feature/ui/MenuFragment.kt
@@ -5,20 +5,99 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kristalcraft.pizzaapp.R
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kristalcraft.delegate_adapter.MainCompositeAdapter
+import com.kristalcraft.pizzaapp.App
+import com.kristalcraft.pizzaapp.databinding.FragmentMenuBinding
+import com.kristalcraft.pizzaapp.dishes_feature.di.DaggerDishesComponent
+import com.kristalcraft.pizzaapp.dishes_feature.di.DishesComponent
+import com.kristalcraft.pizzaapp.dishes_feature.ui.recyclerview.CategoriesAdapterDelegate
+import com.kristalcraft.pizzaapp.dishes_feature.ui.recyclerview.DishAdapterDelegate
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 class MenuFragment : Fragment() {
 
+    private lateinit var categoriesComponent: DishesComponent
+    private lateinit var binding: FragmentMenuBinding
+    private val fragmentCoroutineScope = CoroutineScope(Job() + Dispatchers.Main)
+
+    @Inject
+    lateinit var viewModel: DishesViewModel
+
+    private val categoryAdapter by lazy {
+        MainCompositeAdapter.Builder()
+            .add(CategoriesAdapterDelegate(categoryListener))
+            .build()
+    }
+
+    private val dishAdapter by lazy {
+        MainCompositeAdapter.Builder()
+            .add(DishAdapterDelegate())
+            .build()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+        buildComponent()
+        binding = FragmentMenuBinding.inflate(layoutInflater)
 
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.categoriesRecycler.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL , false )
+        binding.categoriesRecycler.adapter = categoryAdapter
 
+        binding.dishesRecycler.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL , false )
+        binding.dishesRecycler.adapter = dishAdapter
+
+        fragmentCoroutineScope.launch {
+            viewModel.categoriesState.collect{
+                categoryAdapter.submitList(it)
+            }
+        }
+
+        fragmentCoroutineScope.launch {
+            viewModel.dishesState.collect{
+                dishAdapter.submitList(it)
+            }
+        }
+
+        fragmentCoroutineScope.launch {
+            viewModel.messageState.collect{
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun buildComponent() {
+        val appComponent = (context?.applicationContext as App).appComponent
+        categoriesComponent = DaggerDishesComponent
+            .builder()
+            .appComponent(appComponent)
+            .fragment(this)
+            .build()
+        categoriesComponent.inject(this)
+    }
+
+    private val categoryListener = { category: String ->
+        viewModel.getDishes(category)
+    }
+
+    override fun onDestroy() {
+        fragmentCoroutineScope.cancel()
+        super.onDestroy()
     }
 
 }
