b04ece44ca9d674ce5ca59576ffabb4bca00baba
Kristalcraft <dmbezoar@gmail.com>
2023-11-04 11:58:55 +0300

    fix textView cutting

--- app/src/main/java/com/kristalcraft/pizzaapp/dishes_feature/ui/recyclerview/DishAdapterDelegate.kt
+++ app/src/main/java/com/kristalcraft/pizzaapp/dishes_feature/ui/recyclerview/DishAdapterDelegate.kt
@@ -1,13 +1,15 @@
 package com.kristalcraft.pizzaapp.dishes_feature.ui.recyclerview
 
+import android.graphics.Rect
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kristalcraft.delegate_adapter.DelegateAdapter
 import com.kristalcraft.delegate_adapter.DelegateAdapterItem
-import com.kristalcraft.pizzaapp.databinding.CategoryViewholderBinding
 import com.kristalcraft.pizzaapp.databinding.DishViewholderBinding
-import com.kristalcraft.pizzaapp.dishes_feature.domain.model.CategoryModel
 import com.kristalcraft.pizzaapp.dishes_feature.domain.model.DishModel
 
 class DishAdapterDelegate() : DelegateAdapter<DishModel, DishAdapterDelegate.DishViewHolder>(DishModel::class.java) {
@@ -31,6 +33,30 @@ class DishAdapterDelegate() : DelegateAdapter<DishModel, DishAdapterDelegate.Dis
             binding.title.text = item.name
             binding.subtitle.text = item.description
             binding.price.text = "от ${item.price} руб"
+
+            val textView = binding.subtitle
+            minimizeTextViewWhenCut(textView)
         }
+    }
+
+    private fun minimizeTextViewWhenCut(textView: TextView){
+        val observer = textView?.viewTreeObserver
+        observer?.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                textView?.let { view ->
+                    val lineHeight: Int
+                    lineHeight = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        val bounds = Rect()
+                        textView.getLineBounds(0, bounds)
+                        bounds.bottom - bounds.top
+                    } else {
+                        textView.lineHeight
+                    }
+                    val maxLines = textView.height / lineHeight
+                    textView.maxLines = maxLines
+                    textView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            }
+        })
     }
 }
